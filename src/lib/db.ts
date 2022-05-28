c491d30e4549832366a7d57022f97a5613d657f7
pixelmund <pixelmund@gmail.com>
2022-05-28 11:01:02 +0200

    dont know why deploys broke

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,6 @@
-import { dev } from '$app/env';
 import Prisma from '@prisma/client';
 const { PrismaClient } = Prisma;
 
 export const db = new PrismaClient({
-	log: !dev ? [] : ['info', 'query', 'warn', 'error'],
+	log: ['info', 'query', 'warn', 'error']
 });
